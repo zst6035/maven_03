117a6bae36d21433b3ad85671b56274ff0a8becc
zst111 <shouting_zhang@baofu.com>
2020-08-14 16:59:25 +0800

    添加了user4

--- src/main/java/com/bjsxt/servlet/HelloServlet.java
+++ src/main/java/com/bjsxt/servlet/HelloServlet.java
@@ -14,12 +14,16 @@ public class HelloServlet extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         User user1=new User();
         User user2=new User();
+        User user4=new User();
         user1.setId(1);
         user1.setName("hah");
         user1.setAge(12);
         user2.setId(1);
         user2.setName("hah");
         user2.setAge(12);
+        user4.setId(1);
+        user4.setName("hah");
+        user4.setAge(12);
         resp.getWriter().write("hello,"+user1.toString()+","+user2.toString());
     }
 }
